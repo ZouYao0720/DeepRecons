d8e209606c8c78fbd095030b38385cf31cda7200
ZouYao0720 <55712182+ZouYao0720@users.noreply.github.com>
2022-08-06 14:30:15 -0700

    Update trainer.py

--- ISR/train/trainer.py
+++ ISR/train/trainer.py
@@ -14,6 +14,7 @@ from ISR.utils.metrics import PSNR
 from ISR.utils.metrics import PSNR_Y
 from ISR.utils.logger import get_logger
 from ISR.utils.utils import check_parameter_keys
+from ISR.utils.medical_image import MedicalImageHandler
 
 
 class Trainer:
@@ -124,14 +125,14 @@ class Trainer:
             fallback_save_every_n_epochs=fallback_save_every_n_epochs,
         )
 
-        self.train_dh = DataHandler(
+        self.train_dh = MedicalImageHandler(
             lr_dir=lr_train_dir,
             hr_dir=hr_train_dir,
             patch_size=self.lr_patch_size,
             scale=self.scale,
             n_validation_samples=None,
         )
-        self.valid_dh = DataHandler(
+        self.valid_dh = MedicalImageHandler(
             lr_dir=lr_valid_dir,
             hr_dir=hr_valid_dir,
             patch_size=self.lr_patch_size,
