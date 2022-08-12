773c38c03226f7ed91a27be1107973e3516823fa
ihaiwang <110649399+ihaiwang@users.noreply.github.com>
2022-08-12 09:07:08 +0800

    except the vgg part

--- ISR/models/cut_vgg19_2.py
+++ ISR/models/cut_vgg19_2.py
@@ -1,8 +1,7 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.applications.vgg19 import VGG19
-
 from ISR.utils.logger import get_logger
-
+from tensorflow.keras.layers import concatenate, Input, Activation, Add, Conv2D, Lambda, UpSampling2D
 
 class Cut_VGG19:
     """
@@ -21,6 +20,7 @@ class Cut_VGG19:
     def __init__(self, patch_size, layers_to_extract):
         self.patch_size = patch_size
         self.input_shape = (patch_size,) * 2 + (3,)
+        self.input_shape_conv = (patch_size,) * 2 + (1,)
         self.layers_to_extract = layers_to_extract
         self.logger = get_logger(__name__)
         
@@ -36,9 +36,16 @@ class Cut_VGG19:
         layers selected by self.layers_to_extract.
         """
         
+        input_tensor = Input(shape=self.input_shape_conv)
+        x = Conv2D(3,(3,3),padding='same')(input_tensor)    # x has a dimension of (IMG_SIZE,IMG_SIZE,3)
+        
         vgg = VGG19(weights='imagenet', include_top=False, input_shape=self.input_shape)
         vgg.trainable = False
-        outputs = [vgg.layers[i].output for i in self.layers_to_extract]
-        self.model = Model([vgg.input], outputs)
+        
+        x = vgg(x)
+        outputs = [x.layers[i].output for i in self.layers_to_extract]
+        
+
+        self.model = Model(input_tensor, outputs)
         self.model._name = 'feature_extractor'
         self.name = 'vgg19'  # used in weights naming
